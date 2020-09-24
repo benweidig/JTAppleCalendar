2560ef6a6e7c4b6b9ecccc3ff88516154843abf4
Benjamin Weidig <ben@netzgut.net>
2020-09-24 09:10:20 +0200

    trying to make SPM work

--- Package.swift
+++ Package.swift
@@ -1,7 +1,19 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-
 let package = Package(
-    name: "JTAppleCalendar"
+    name: "JTAppleCalendar",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "JTAppleCalendar",
+            targets: ["JTAppleCalendar"]),
+    ],
+    targets: [
+        .target(
+            name: "JTAppleCalendar",
+            dependencies: [])
+    ]
 )
